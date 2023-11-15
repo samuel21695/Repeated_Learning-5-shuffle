9df08448bf1e7e2b8c90af4158d786a3f385dbe7
samuel21695 <samuel21695@gmail.com>
2023-11-15 21:45:52 +0900

    ✨ function getRandomIndex

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * 주어진 배열을 무작위로 섞는 함수
+ * @param {Array} array - 섞을 배열
+ * @returns {Array} 무작위로 섞인 배열
+ */
+
+function shuffleArray(array) {
+  /**
+   * 주어진 최대값 내에서 무작위 인덱스를 반환하는 함수.
+   * @param {number} max - 무작위 인덱스를 생성할 때 고려할 최대 범위.
+   * @returns {number} 생성된 무작위 인덱스.
+   */
+  function getRandomIndex(max) {
+    return Math.floor(Math.random() * max); // 0과 max 사이에 랜덤한 정수 반환
+  }
+
+  // array의 모든 요소에 대해 루프를 돌며 무작위 위치화 교환
+}
