bd750a20855cf6f5c02b574a971eb7923ef52379
samuel21695 <samuel21695@gmail.com>
2023-11-15 22:08:08 +0900

    ✨ function shuffleArray

--- app.js
+++ app.js
@@ -14,5 +14,15 @@ function shuffleArray(array) {
     return Math.floor(Math.random() * max); // 0과 max 사이에 랜덤한 정수 반환
   }
 
-  // array의 모든 요소에 대해 루프를 돌며 무작위 위치화 교환
+  // array의 모든 요소에 대해 루프를 돌며 무작위 위치와 교환
+  array.forEach((currentValue, index) => {
+    const randomIndex = getRandomIndex(array.length); //랜덤 인덱스 생성
+
+    // 요소 교환(swap): 배열 내의 두 요소의 위치를 교환
+    const temp = array[index]; //현재 요소 임시 저장
+    array[index] = array[randomIndex]; // 현재 위치에 랜덤 위치의 요소 저장
+    array[randomIndex] = temp; // 랜덤 위치에 임시 저장한 요소 저장
+  });
+
+  return array; // 섞인 배열 반환
 }
